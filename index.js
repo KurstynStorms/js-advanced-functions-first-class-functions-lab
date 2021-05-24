a61716c119d1c7445013a43d7dca446c6055a094
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-05-23 20:21:23 -0400

    Advanced Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers (drivers) {
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice (-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers ,returnLastTwoDrivers]
+
+function createFareMultiplier(){
+    return function(){
+    }
+}
+function createFareMultiplier(fareQuintupler){
+    return function(){
+        return fareQuintupler * fareQuintupler
+    }
+}
+function fareDoubler(doubleFare){
+    return doubleFare * 2
+}
+function fareTripler(tripleFare){
+    return tripleFare * 3
+}
+function selectDifferentDrivers(drivers, kurstyn){
+    return kurstyn(drivers)
+}
